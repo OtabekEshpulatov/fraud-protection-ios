c191c3185ac99cf071a6515f0d14adc60c8acdf7
mavlon <mavlon@zeroone.uz>
2025-04-20 22:35:11 +0200

    Initial Commit

---
+++ FraudProtection/FraudProtectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FraudProtectionApp.swift
+//  FraudProtection
+//
+//  Created by Mavlon Ergashev on 20/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FraudProtectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
